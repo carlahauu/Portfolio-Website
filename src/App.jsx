2d026967ba5fef547a0459686384376e0848ac6f
Carla Hau <carlahau@Carlas-Air.attlocal.net>
2024-07-12 02:42:23 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import styles from './App.module.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className={styles.App}>Hello world!</div>
+  )
+}
+
+export default App
